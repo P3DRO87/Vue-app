d19f559589004abf70847cd0056fa47c42291a51
antoniofloresonline <wanllyf@gmail.com>
2020-03-24 15:18:47 -0600

    adding artist list

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@ const app = new Vue({
   el: "#app",
   data: {
     titulo: "Vue is ready!",
-    artists: ["skrillex", "one republic", "barnie"]
+    artists: ["skrillex", "one republic", "barnie"],
+    apiUrl:
+      "https://dev-encore-development.pantheonsite.io/wp-json/wp/v2/artists"
+  },
+
+  methods: {
+    getArtists() {
+      axios.get(this.apiUrl).then(response => {
+        this.artists = response.data;
+        console.log(response);
+      });
+    }
+  },
+
+  created() {
+    this.getArtists();
   }
 });
