a66a29beb42ae17ee0b82281681bfd204ba1ae39
antoniofloresonline <wanllyf@gmail.com>
2020-03-24 15:29:35 -0600

    Fixing console errors

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 const app = new Vue({
   el: "#app",
   data: {
-    titulo: "Vue is ready!",
-    artists: ["skrillex", "one republic", "barnie"],
+    titulo: "Artists List",
+    artists: [],
     apiUrl:
       "https://dev-encore-development.pantheonsite.io/wp-json/wp/v2/artists"
   },
-
   methods: {
     getArtists() {
       axios.get(this.apiUrl).then(response => {
         this.artists = response.data;
-        console.log(response);
+        // console.log(response);
       });
     }
   },
-
   created() {
     this.getArtists();
   }
